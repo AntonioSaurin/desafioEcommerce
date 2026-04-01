b70bb91dd04d009fe32e124aa5d493ebabf85d6c
AntonioSaurin <antonio.saurin@etec.sp.gov.br>
2026-04-01 16:13:54 -0300

    feat: implementa visualizacao, edicao e exclusao logica de produtos

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -3,10 +3,36 @@ const prisma = require('../models/prisma');
 class ProductController{
     async index(req, res){
         try{
-            const products = await prisma.product.findMany();
+            const products = await prisma.product.findMany({
+                where: {
+                    active: true
+                }
+            });
             return res.json(products);
         }catch{
             return res.status(500).json({error: 'Falha ao buscar produtos!'});
+        }
+    }
+
+    async show(req, res){
+        try {
+            const {id} = req.params;
+
+            const product = await prisma.product.findFirst({
+                where:{
+                    id,
+                    active: true
+                }
+            })
+
+            if(!product){
+                return res.status(404).json({error: 'Produto nao encontrado'})
+            }
+
+            return res.json(product);
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({error: 'Erro ao procurar produto'});
         }
     }
 
@@ -32,6 +58,67 @@ class ProductController{
         }catch(error){
             console.error(error);
             return res.status(500).json({error: 'Erro ao criar produto'});
+        }
+    }
+
+    async update(req, res){
+        try{
+            const {id} = req.params;
+            const {name, description, price, stock, category} = req.body;
+            
+            const productExists = await prisma.product.findUnique({
+                where: {
+                    id,
+                    active: true
+                }
+
+            });
+
+            if(!productExists){
+                return res.status(404).json({error: 'Produto nao encontrado'});
+            }
+
+            const product = await prisma.product.update({
+                where: {id},
+                data: {
+                    name,
+                    description,
+                    price: price ? parseFloat(price) : undefined,
+                    stock: stock !== undefined ?parseInt(stock) : undefined,
+                    category
+                }
+            });
+
+            return res.json(product);
+        }catch(error){
+            console.error(error);
+            return res.status(500).json({error: 'Erro ao atualizar produto'})
+        }
+    }
+
+    async delete(req, res){
+        try {
+            const {id} = req.params;
+
+            const productExists = await prisma.product.findUnique({
+                where: {id},
+            })
+
+            if(!productExists){
+                return res.status(404).json({error: 'Produto nao encontrado'});
+            }
+
+            await prisma.product.update({
+                where: {id},
+                data:{
+                    active: false,
+                }
+            });
+
+            return res.status(204).send();
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({error: 'Erro ao deletar produto'});
         }
     }
 }
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -4,6 +4,9 @@ const authMiddleware = require('../middlewares/authMiddleware');
 const routes = Router();
 
 routes.get('/products', ProductController.index);
+routes.get('/products/:id', ProductController.show);
 routes.post('/products', authMiddleware, ProductController.create);
+routes.put('/products/:id', authMiddleware, ProductController.update);
+routes.delete('/products/:id', authMiddleware, ProductController.delete);
 
 module.exports = routes;
